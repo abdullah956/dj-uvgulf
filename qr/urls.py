8f66feec33d327f599548e45e9e973ce1ee065fe
abdullah956 <abdullaharshed956@gmail.com>
2025-04-10 19:39:04 +0500

    auth

--- qr/urls.py
+++ qr/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import add_certification, certification_list, generate_qr_code, certificate_details
+from .views import add_certification, certification_list, generate_qr_code, certificate_details, login_view, logout_view
 
 urlpatterns = [
     path('add_certification/', add_certification, name='add_certification'),
     path('certifications/', certification_list, name='certification_list'),
     path('qr_code/<str:tac_number>/', generate_qr_code, name='generate_qr_code'),
     path('certificate/<str:tac_number>/', certificate_details, name='certificate_details'),  # New URL for certificate details
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
--- qr/views.py
+++ qr/views.py
@@ -4,7 +4,11 @@ from .models import OperatorCertification
 import qrcode
 from io import BytesIO
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+@login_required
 def add_certification(request):
     if request.method == 'POST':
         form = OperatorCertificationForm(request.POST, request.FILES)
@@ -16,6 +20,7 @@ def add_certification(request):
 
     return render(request, 'add_certification.html', {'form': form})
 
+@login_required
 def certification_list(request):
     certifications = OperatorCertification.objects.all()
     return render(request, 'certification_list.html', {'certifications': certifications})
@@ -38,10 +43,29 @@ def generate_qr_code(request, tac_number):
     # Return the image as an HTTP response
     return HttpResponse(qr_io, content_type='image/png')
 
-
+@login_required
 def certificate_details(request, tac_number):
     # Retrieve the certificate using the TAC number
     certificate = get_object_or_404(OperatorCertification, tac_number=tac_number)
     
     # Render the certificate details page
     return render(request, 'certificate_details.html', {'certificate': certificate})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("certification_list")  # Redirect to home page after login
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "login.html")
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("login")  # Redirect to login page after logout
